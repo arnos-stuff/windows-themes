82681a3c422209a7c119c0dfe2f11412ff0fbf5f
Arno V <118028859+arnos-stuff@users.noreply.github.com>
2023-04-05 02:47:21 +0200

    Update prepare.ps1

--- prepare.ps1
+++ prepare.ps1
@@ -1,3 +1,5 @@
+mkdir tmp ; 
+cd tmp;
 
 go install jreisinger/ghrel@latest ;
 
@@ -13,4 +15,3 @@ git clone https://github.com/niivu/Windows-11-themes.git Windows-Themes ;
 
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 dir *ttf*/*.ttf | %{ $fonts.CopyHere($_.fullname) } -Confirm ;
-

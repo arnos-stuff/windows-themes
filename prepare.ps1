daab2f36127e42ec2007c6a088a2887110861519
Arno V <118028859+arnos-stuff@users.noreply.github.com>
2023-04-05 02:46:15 +0200

    Update prepare.ps1

--- prepare.ps1
+++ prepare.ps1
@@ -12,4 +12,5 @@ ghrel rainmeter/rainmeter ;
 git clone https://github.com/niivu/Windows-11-themes.git Windows-Themes ; 
 
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
-dir fonts/*.ttf | %{ $fonts.CopyHere($_.fullname) }
+dir *ttf*/*.ttf | %{ $fonts.CopyHere($_.fullname) } -Confirm ;
+

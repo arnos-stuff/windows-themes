18712fc47813c06dc4966b675ad9f0532d0d79b1
Arno V <118028859+arnos-stuff@users.noreply.github.com>
2023-04-05 02:39:59 +0200

    Create prepare.ps1

---
+++ prepare.ps1
@@ -0,0 +1,15 @@
+
+go install jreisinger/ghrel@latest ;
+
+ghrel tonsky/FiraCode;
+
+7x *.zip ;
+
+ghrel namazso/SecureUxTheme ;
+
+ghrel rainmeter/rainmeter ;
+
+git clone https://github.com/niivu/Windows-11-themes.git Windows-Themes ; 
+
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+dir fonts/*.ttf | %{ $fonts.CopyHere($_.fullname) }

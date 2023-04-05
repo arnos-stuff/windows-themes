c5522f1d0222e0e49d4b0b6f381f61fa661032a4
Arno V <118028859+arnos-stuff@users.noreply.github.com>
2023-04-05 02:41:26 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -12,7 +12,7 @@ scoop install rust ;
 scoop install vcredist2022 ; 
 scoop install starship;
 
-go install jreisinger/ghrel@latest ;
+go install github.com/jreisinger/ghrel@latest ;
 
 ghrel tonsky/FiraCode;
 
